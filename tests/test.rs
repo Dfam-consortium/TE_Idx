8136509c06701237b017b47bdbcb04afd1616ae5
asgray <antgray89@gmail.com>
2024-05-15 17:55:13 -0700

    tests almost done

--- tests/test.rs
+++ tests/test.rs
@@ -1,14 +1,15 @@
 use noodles::bgzf;
-use std::fs::{read_dir, File};
-use std::io::{BufRead, BufReader, stdout, BufWriter, Write};
+use serde_json::from_str;
+use std::fs::{read_dir, read_to_string, File};
+use std::io::{BufRead, BufReader, Read};
 use std::path::Path;
-use te_idx::idx::{build_idx, prep_idx, search_idx};
+use te_idx::idx::{build_idx, prep_idx};
 use te_idx::{
     bgzf_filter, idx_query, json_query, prep_beds, prepare_assembly, process_json,
-    read_family_assembly_annotations, MASKS_DIR, SEQUENCE_DIR, ASSEMBLY_DIR, Annotation
+    read_family_assembly_annotations, Annotation, ASSEMBLY_DIR, BENCHMARK_DIR, MASKS_DIR,
+    MOD_LEN_DIR, SEQUENCE_DIR,
 };
 use tempfile::{NamedTempFile, TempDir};
-use serde_json::from_str;
 
 pub const TEST_DIR: &'static str = "/home/agray/te_idx/tests";
 pub const TEST_DATA_DIR: &'static str = "/home/agray/te_idx/tests/data";
@@ -49,10 +50,7 @@ fn test_bgzf_filter_nrph() {
             let orig_count = BufReader::new(
                 File::open(format!(
                     "{}/{}/{}/{}.bed.bgz",
-                    TEST_DATA_DIR,
-                    TEST_ASSEMBLY,
-                    ASSEMBLY_DIR,
-                    fam
+                    TEST_DATA_DIR, TEST_ASSEMBLY, ASSEMBLY_DIR, fam
                 ))
                 .expect("Can't Open File"),
             )
@@ -98,10 +96,7 @@ fn test_bgzf_filter_dl_fmt() {
             let orig_count = BufReader::new(
                 File::open(format!(
                     "{}/{}/{}/{}.bed.bgz",
-                    TEST_DATA_DIR,
-                    TEST_ASSEMBLY,
-                    ASSEMBLY_DIR,
-                    fam
+                    TEST_DATA_DIR, TEST_ASSEMBLY, ASSEMBLY_DIR, fam
                 ))
                 .expect("Can't Open File"),
             )
@@ -148,14 +143,17 @@ fn test_build_idx() {
         };
     assert_eq!(filenames.len(), 341);
     assert_eq!(bgz_dir, "/home/agray/te_idx/tests/data/test_ex/masks");
-    assert_eq!(index_file, "/home/agray/te_idx/tests/data/test_ex/masks_idx.dat");
+    assert_eq!(
+        index_file,
+        "/home/agray/te_idx/tests/data/test_ex/masks_idx.dat"
+    );
 
     let working_directory = gen_working_dir();
     let test_data_dir = working_directory.path().to_str().unwrap();
     let test_index_file = format!("{}/masks_idx.dat", test_data_dir);
 
     build_idx(&filenames, &bgz_dir, &mut contig_index, &test_index_file).expect("Indexing Failed");
-    
+
     assert!(Path::new(&test_index_file).exists());
 
     let _ = working_directory.close();
@@ -172,12 +170,32 @@ fn test_idx_query() {
     let nrph = &false;
     let data_directory = Some(TEST_DATA_DIR);
 
-    let res1 = idx_query(assembly, data_type, chrom, start, end, family, nrph, data_directory).expect("Index Query Failed");
+    let res1 = idx_query(
+        assembly,
+        data_type,
+        chrom,
+        start,
+        end,
+        family,
+        nrph,
+        data_directory,
+    )
+    .expect("Index Query Failed");
     let vals1: Vec<Annotation> = from_str(&res1).expect("Cannot Deserialize");
     assert_eq!(vals1.len(), 32);
 
-    let family = &Some("DF000000001".to_string());    
-    let res2 = idx_query(assembly, data_type, chrom, start, end, family, nrph, data_directory).expect("Index Query Failed");
+    let family = &Some("DF000000001".to_string());
+    let res2 = idx_query(
+        assembly,
+        data_type,
+        chrom,
+        start,
+        end,
+        family,
+        nrph,
+        data_directory,
+    )
+    .expect("Index Query Failed");
     let vals2: Vec<Annotation> = from_str(&res2).expect("Cannot Deserialize");
     assert_eq!(vals2.len(), 15);
 
@@ -195,8 +213,10 @@ fn test_json_query() {
     let key = &"1".to_string();
     let target = &"length".to_string();
     let data_directory = Some(TEST_DATA_DIR);
-    let ans = json_query(assembly, data_type, key, target, data_directory).expect("JSON Read Failed");
-    assert_eq!(ans , "248956422");
+
+    let ans =
+        json_query(assembly, data_type, key, target, data_directory).expect("JSON Read Failed");
+    assert_eq!(ans, "248956422");
 }
 
 #[test]
@@ -228,22 +248,123 @@ fn test_prep_beds() {
                     .unwrap_or(0)
             );
         }
-        Err(_e) =>  {let _ = working_directory.close();panic!()},
+        Err(_e) => {
+            let _ = working_directory.close();
+            panic!()
+        }
     }
     let _ = working_directory.close();
 }
 
 #[test]
 fn test_prepare_assembly() {
-    panic!()
+    let working_directory = gen_working_dir();
+    let test_data_dir = Some(working_directory.path().to_str().unwrap());
+
+    let assembly = &TEST_ASSEMBLY.to_string();
+    let data_directory = test_data_dir;
+    let export_directory = Some(TEST_EXPORT_DIR);
+
+    let _ = prepare_assembly(assembly, data_directory, export_directory);
+    let assembly_dir = format!("{}/{}", data_directory.unwrap(), TEST_ASSEMBLY);
+
+    let align_dir = &format!("{}/{}", assembly_dir, ASSEMBLY_DIR);
+    // let mut align_contents = Path::new(&align_dir).read_dir().expect("Couldn't Read Dir");
+    let bench_dir = &format!("{}/{}", assembly_dir, BENCHMARK_DIR);
+    // let mut bench_contents = Path::new(&bench_dir).read_dir().expect("Couldn't Read Dir");
+    let mask_dir = &format!("{}/{}", assembly_dir, MASKS_DIR);
+    // let mut mask_contents = Path::new(&mask_dir).read_dir().expect("Couldn't Read Dir");
+    let mlen_dir = &format!("{}/{}", assembly_dir, MOD_LEN_DIR);
+    // let mut mlen_contents = Path::new(&mlen_dir).read_dir().expect("Couldn't Read Dir");
+    let seq_dir = &format!("{}/{}", assembly_dir, SEQUENCE_DIR);
+    // let mut seq_contents = Path::new(&seq_dir).read_dir().expect("Couldn't Read Dir");
+
+    assert!(Path::new(align_dir).exists());
+    assert!(Path::new(bench_dir).exists());
+    assert!(Path::new(mask_dir).exists());
+    assert!(Path::new(mlen_dir).exists());
+    assert!(Path::new(seq_dir).exists());
+    assert!(Path::new(&format!("{}/{}_idx.dat", assembly_dir, ASSEMBLY_DIR)).exists());
+    assert!(Path::new(&format!("{}/{}_idx.dat", assembly_dir, BENCHMARK_DIR)).exists());
+    assert!(Path::new(&format!("{}/{}_idx.dat", assembly_dir, MASKS_DIR)).exists());
+    // assert!(!align_contents.next().is_none());
+    // assert!(!bench_contents.next().is_none());
+    // assert!(!mask_contents.next().is_none());
+    // assert!(!mlen_contents.next().is_none());
+    // assert!(!seq_contents.next().is_none());
+    //TODO fix
+
+    let _c = working_directory.close();
 }
 
 #[test]
 fn test_process_json() {
-    panic!()
+    let out_f = NamedTempFile::new_in(TEST_DATA_DIR).expect("Couldn't Open Output File");
+
+    let in_file = &format!(
+        "{}/{}/{}-{}.json",
+        TEST_EXPORT_DIR, TEST_ASSEMBLY, TEST_ASSEMBLY, SEQUENCE_DIR
+    );
+    let key = &"accession".to_string();
+    let outfile = &Some(out_f.path().to_str().unwrap().to_string());
+
+    process_json(in_file, key, outfile).expect("JSON Parse Failed");
+
+    let output = read_to_string(out_f).expect("Can't Read File");
+    let comparison = format!(
+        "{}/{}/{}/{}-processed-{}.json",
+        TEST_DATA_DIR, TEST_ASSEMBLY, SEQUENCE_DIR, TEST_ASSEMBLY, SEQUENCE_DIR
+    );
+    let target = read_to_string(comparison).expect("Can't Read File");
+    assert_eq!(output, target);
 }
 
 #[test]
 fn test_read_family_assembly_annotation() {
-    panic!()
+    let out_f = NamedTempFile::new_in(TEST_DATA_DIR).expect("Couldn't Open Output File");
+
+    let id = &"DF000000001".to_string();
+    let assembly_id = &TEST_ASSEMBLY.to_string();
+    let nrph = &false;
+    let outfile = &Some(out_f.path().to_str().unwrap().to_string());
+    let data_directory = Some(TEST_DATA_DIR);
+
+    let _ = read_family_assembly_annotations(id, assembly_id, nrph, outfile, data_directory);
+
+    let mut decompressed = Vec::new();
+    let mut reader = bgzf::Reader::new(File::open(out_f).expect("can't open"));
+    reader.read_to_end(&mut decompressed).expect("Can't Read");
+    let output = String::from_utf8_lossy(&decompressed);
+    let target = read_to_string(Path::new(&format!(
+        "{}/DF000000001.test.tsv",
+        TEST_DATA_DIR
+    )))
+    .expect("Can't Read File");
+
+    assert_eq!(output, target)
+}
+
+#[test]
+fn test_read_family_assembly_annotation_nrph() {
+    let out_f = NamedTempFile::new_in(TEST_DATA_DIR).expect("Couldn't Open Output File");
+
+    let id = &"DF000000001".to_string();
+    let assembly_id = &TEST_ASSEMBLY.to_string();
+    let nrph = &true;
+    let outfile = &Some(out_f.path().to_str().unwrap().to_string());
+    let data_directory = Some(TEST_DATA_DIR);
+
+    let _ = read_family_assembly_annotations(id, assembly_id, nrph, outfile, data_directory);
+
+    let mut decompressed = Vec::new();
+    let mut reader = bgzf::Reader::new(File::open(out_f).expect("can't open"));
+    reader.read_to_end(&mut decompressed).expect("Can't Read");
+    let output = String::from_utf8_lossy(&decompressed);
+    let target = read_to_string(Path::new(&format!(
+        "{}/DF000000001.test.nrph.tsv",
+        TEST_DATA_DIR
+    )))
+    .expect("Can't Read File");
+
+    assert_eq!(output, target)
 }
